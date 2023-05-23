f9bb93a8001e227165b50df1a5625b43b4eda19a
Muhammad Islam Kamran <50517711+islamkamran@users.noreply.github.com>
2023-05-23 11:09:43 +0500

    Practice of JS

---
+++ index.js
@@ -0,0 +1,63 @@
+// // variables
+// let name;
+// name="islam"; 
+// console.log(name)
+
+// let firstName;
+// firstName = "Muhammad";
+// console.log(firstName )
+
+// // constants
+// const interestRate = 0.3;
+// console.log(interestRate);
+
+
+// //Primitive types
+// let lastName = 'Kamran';
+// let age = 27;
+// let isApproved = true;
+// let selectedColor = undefined;
+// let middleName = null; 
+
+
+// object
+// let person = {
+//     name : 'islam',
+//     age:27
+
+// };
+// // dot notation more concise and easy to use
+// // person.name = "kami kami"
+
+// // bracket notation
+// person['name']="Muhammad";
+// console.log(person );  
+
+
+// ARRAYS
+// List of Objects
+// let selectedColors = ['red','blue'];//[array literals]
+// //expanding and mu;tiple types
+// selectedColors[2] = "burgande";
+// selectedColors[3]=1;
+// console.log(selectedColors)
+
+// console.log(selectedColors.length); 
+
+
+//functions in JS
+//performing a task
+// function greet(person){//parameter
+//     // body of function
+//     console.log("hello " + person);
+// }
+// let person = "Muhammad";
+// greet(person);//argument
+// person = "Wali";
+// greet(person);
+
+function square(number){
+    return number*number;
+}
+
+console.log("the square: "+ square(2))
